702c3b3c5b9bbae801c3ac9ac8a77989578e8139
jleprince <jeremy.leprince1@gmail.com>
2017-12-02 04:15:10 +0100

    Initial - working empty app

---
+++ src/AppBundle/Controller/ForumController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+class ForumController extends Controller
+{
+
+
+    public function indexAction()
+    {
+        return $this->render('AppBundle:Forum:index.html.twig');
+    }
+
+}
